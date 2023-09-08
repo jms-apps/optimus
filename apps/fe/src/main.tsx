def9d73c573d9b30169ae41a3c6276a498c18f95
Jimish Shah <jimish.shah@itv.com>
2023-09-08 17:24:22 +0100

    wip

--- apps/fe/src/main.tsx
+++ apps/fe/src/main.tsx
@@ -1,13 +1,30 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import resolveConfig from 'tailwindcss/resolveConfig';
+// import tailwindConfig from '../tailwind.config.js';
 
 import App from './app/app';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// const fullConfig = resolveConfig(tailwindConfig);
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: { main: '#0a2463' }, // 254E70 2274A5 // use rubik fonts
+  },
+  // typography: {
+  //   fontf
+  //   fontFamily: ['Rubik'],
+  // },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
