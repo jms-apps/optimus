49c85cd98eec035d8c8cb8be0a63298b26a0cdb4
Jimish Shah <jimish.shah@itv.com>
2023-09-13 12:53:37 +0100

    BE available on subdomain

--- apps/be/lib/core/core-stack.ts
+++ apps/be/lib/core/core-stack.ts
@@ -3,7 +3,14 @@ import { Construct } from 'constructs';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
 import { UserPool, VerificationEmailStyle } from 'aws-cdk-lib/aws-cognito';
 import path = require('path');
+import { CnameRecord, HostedZone } from 'aws-cdk-lib/aws-route53';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
+const DOMAIN_NAME = 'sujanashah.com';
+const CNAME = `api.${DOMAIN_NAME}`;
+const ACM_CERT_ARN =
+  'arn:aws:acm:us-east-1:618246572188:certificate/bbf76ebd-498e-4bae-b85a-7df1e6f764ef';
+const HOSTED_ZONE_ID = 'Z1089WFICFNFEM';
 export class CoreStack extends cdk.Stack {
   public api: cdk.aws_appsync.GraphqlApi;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -27,12 +34,39 @@ export class CoreStack extends cdk.Stack {
       },
     });
 
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      'sujanashah.comCert',
+      ACM_CERT_ARN
+    );
+
     this.api = new appsync.GraphqlApi(this, 'OptimusGraphql', {
       name: 'optimusGraphql',
       definition: appsync.Definition.fromFile(
         path.join(__dirname, '../schema.graphql')
       ),
       xrayEnabled: true,
+      domainName: {
+        certificate,
+        domainName: CNAME,
+      },
+    });
+
+    // hosted zone for adding appsync domain
+    const hostedZone = HostedZone.fromHostedZoneAttributes(
+      this,
+      `Sujanashah.comZone`,
+      {
+        hostedZoneId: HOSTED_ZONE_ID,
+        zoneName: DOMAIN_NAME,
+      }
+    );
+
+    // create a cname to the appsync domain. will map to something like xxxx.cloudfront.net
+    new CnameRecord(this, `optimusApiRecord`, {
+      recordName: CNAME,
+      zone: hostedZone,
+      domainName: this.api.appSyncDomainName,
     });
   }
 }
