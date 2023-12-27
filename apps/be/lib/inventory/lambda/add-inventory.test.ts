90b5413d9738f2051f46f819380bedc04eb8655a
Jimish Shah <jimish.shah@prettylittlething.com>
2023-12-27 21:51:01 +0000

    Refactor AWS SDK imports and mock in add-inventory.test.js

--- apps/be/lib/inventory/lambda/add-inventory.test.ts
+++ apps/be/lib/inventory/lambda/add-inventory.test.ts
@@ -1,6 +1,44 @@
 import { handler as addInventoryHandler } from './add-inventory';
 import { MutationAddInventoryArgs } from '@optimus/common';
 import { AppSyncResolverEvent } from 'aws-lambda';
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { vi } from 'vitest';
+
+const mockDocumentClient = {
+  get: ({ TableName, Key: { sku } }) => {
+    if (sku === 'already-exists-sku') {
+      return {
+        promise: () => Promise.resolve({ Item: { sku } }),
+      };
+    }
+    return {
+      promise: () => Promise.resolve({}),
+    };
+  },
+  put: vi.fn().mockReturnThis(),
+  promise: vi.fn(),
+};
+
+vi.mock('aws-sdk', () => {
+  const actual = vi.importActual('aws-sdk');
+  return {
+    ...actual,
+    DynamoDB: {
+      ...actual.DynamoDB,
+      DocumentClient: vi.fn(() => mockDocumentClient),
+    },
+    CognitoIdentityServiceProvider: vi.fn(() => ({
+      getUser: ({
+        AccessToken,
+      }: CognitoIdentityServiceProvider.GetUserRequest) => ({
+        promise: () => {
+          if (AccessToken === 'invalid-token')
+            throw new Error('Token is not valid');
+        },
+      }),
+    })),
+  };
+});
 
 describe('addInventoryHandler', () => {
   it('should return an inventory item if adding is successful', async () => {
@@ -12,6 +50,11 @@ describe('addInventoryHandler', () => {
           price: 1,
         },
       },
+      request: {
+        headers: {
+          'x-auth-token': 'valid-token'
+        }
+      }
     } as unknown as AppSyncResolverEvent<MutationAddInventoryArgs>);
     expect(result).toEqual({
       name: 'test',
@@ -20,6 +63,7 @@ describe('addInventoryHandler', () => {
       sku: expect.any(String),
     });
   });
+
 });
 
 //
